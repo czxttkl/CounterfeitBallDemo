c35211da5f0cecb3372dfeecc275f7e93c6e49b6
czxttkl <czxttkl@gmail.com>
2014-01-08 13:38:22 -0500

    code clean

--- src/io/czxttkl/github/tdt/Leaf.java
+++ src/io/czxttkl/github/tdt/Leaf.java
@@ -4,7 +4,6 @@ public class Leaf implements Node{
 	public int counterfeit_ball;
 	public String weight;
 	
-	
 	public Leaf(int ball, String weight) {
 		this.counterfeit_ball = ball;
 		this.weight = weight;
--- src/io/czxttkl/github/tdt/TernaryDecisionTree.java
+++ src/io/czxttkl/github/tdt/TernaryDecisionTree.java
@@ -3,7 +3,7 @@ package io.czxttkl.github.tdt;
 public class TernaryDecisionTree implements Node{
 	public int[] left_balls;
 	public int[] right_balls;
-	public Object heavier;
-	public Object lighter;
-	public Object equal;
+	public Node heavier;
+	public Node lighter;
+	public Node equal;
 }
